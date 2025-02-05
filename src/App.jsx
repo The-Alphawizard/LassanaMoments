61a81f0ac83b1e2cee432c120ab2b25241404034
Werosh Kriyanjala <weroshprofy@gmail.com>
2025-02-05 12:49:41 +0530

    colors adding

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      <h1 className=' w-full h-screen items-center justify-center flex font-primary  text-primaryText text-5xl font-bold bg-gradient-to-b from-periwinkleBlue to-aliceBlue '>
+        hello
+      </h1>
       
     </div>
   )
